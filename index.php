0403df15605a43da82b5d84db57f1f4afe8ee421
follower <vitfollower@gmail.com>
2015-05-01 17:16:02 +0300

    initial commit

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
-	error_reporting(E_ALL);
-	ini_set('display_errors', false);
-	date_default_timezone_set('Europe/Kiev');
+require_once('vendor/autoload.php');
+require_once('setup.php');
 
-	define('SITE_PATH', dirname(__FILE__)."/");
-	define('CLASSES', dirname(__FILE__)."/protected/classes/");
-	define('CONROLLERS', dirname(__FILE__)."/protected/controllers/");
-	define('OBJECTS', dirname(__FILE__)."/protected/objects/");
-	define('MODULES', dirname(__FILE__)."/protected/modules/");
-
-	require_once(CLASSES."initializer.php");
+$project = new \Accounting\EntryPoint\Base();
+$project->init();
--- protected/controllers/BaseController.php
+++ protected/controllers/BaseController.php
@@ -3,63 +3,6 @@
 class BaseController
 {
 
-	protected $registry;
-	protected $params;
-	protected $key_lang = "ru";
-
-	function  __construct($registry, $params)
-	{
-		global $db;
-		$this->db =& $db;
-	}
-
-	public function Render($param = array())
-	{
-		if (!isset($_SESSION['user_id']) || $_SESSION['user_id'] == '') {
-			if (isset($_COOKIE['userid']) && (int)$_COOKIE['userid'] != 0) {
-				$row = $this->db->row("SELECT id, name FROM `users` WHERE id=?", array($_COOKIE['userid']));
-				if ($row) {
-					$_SESSION['user_id'] = $row['id'];
-					$_SESSION['name'] = $row['name'];
-					setcookie('userid', $row['id'], time() + 2592000);
-					header("Location: /");
-				}
-			} else {
-				$this->loginAction();
-			}
-		}
-
-		if (!isset($this->params['topic'])) $param['topic'] = '';
-		else $param['topic'] = $this->params['topic'];
-
-		$data = $param;
-
-		$view = new View($this->registry);
-		$data['meta']['title'] = 'Бухгалтерия 0.9.0';
-
-		$data['styles'] = $view->Load(array(
-			'style.css',
-			'bootstrap.css',
-			'datepicker.css'
-		,), 'styles');
-
-		$data['scripts'] = $view->Load(array(
-			'jquery.min.js',
-			'scripts.js',
-			'bootstrap.min.js',
-			'datepicker.js',
-			'jquery.flot.min.js',
-			'jquery.flot.canvas.min.js',
-			'jquery.flot.time.min.js',
-			'jquery.flot.pie.min.js'), 'scripts');
-
-		if (isset($_SESSION['user_id'])) {
-			$data['typesArray'] = $this->getTypes();
-			$data['types'] = $this->showTypes($data['typesArray']);
-		}
-
-		return ($view->Render("index.phtml", $data));
-	}
 
 	public function loginAction()
 	{
@@ -72,25 +15,6 @@ class BaseController
 				header("Location: /");
 			}
 		}
-	}
-
-	protected function getTypes()
-	{
-		$types = $this->db->rows("SELECT c.* FROM `Category` c
-								 RIGHT JOIN `user_categories` uc ON c.`id` = uc.`category_id`
-								 WHERE uc.`user_id`=?
-								 ORDER BY c.`name`", array((int)$_SESSION['user_id']));
-
-		return $types;
-	}
-
-	protected function showTypes($types = array())
-	{
-		$result = '';
-		foreach ($types as $val) {
-			$result .= '<option value="' . $val['id'] . '" >' . $val['name'] . '</option>';
-		}
-		return $result;
 	}
 
 	protected function getSum($type, $from, $to, $user_id)
