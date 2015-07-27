6ed4e979ab0ddb31e23c852ddd3952547b062d1f
follower <vitfollower@gmail.com>
2015-07-27 21:06:57 +0300

    update admin repo

--- protected/Controller/Error.php
+++ protected/Controller/Error.php
@@ -9,7 +9,6 @@ class Error extends Base
 		header("HTTP/1.0 404 Not Found");
 
 		$vars = ['title' => 'Page not found'];
-		$data['content'] = $this->view->render('public/templates/404.phtml', $vars);
-		return $this->render($data);
+		return $this->view->render('public/templates/404.phtml', $vars);
 	}
 }
